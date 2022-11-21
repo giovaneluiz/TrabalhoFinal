08bd15a57c5e2272eeec634da054b1dc32645d92
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-11-21 11:23:14 -0300

    classes movidas para pasta model

---
+++ src/br/com/trabalhofinal/model/Login.java
@@ -0,0 +1,39 @@
+package br.com.trabalhofinal.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Login extends Usuario {
+
+  public Login(String email, String senha) {
+    super(email, senha);
+  }
+
+  private final static String LOGIN_QUERY = "SELECT email, TRIM(senha) AS senha FROM usuario WHERE email = ?";
+
+  public boolean logar() {
+    Connection conn = Conexao.connect();
+    boolean logado = false;
+
+    try {
+      PreparedStatement pstmt = conn.prepareStatement(LOGIN_QUERY);
+      pstmt.setString(1, getEmail());
+      ResultSet rs = pstmt.executeQuery();
+
+      if (rs.next()) {
+        String senhaBD = rs.getString("senha");
+        if (senhaBD.equals(getSenha())) {
+          logado = true;
+          conn.close();
+        }
+      }
+
+    } catch (SQLException e) {
+      Conexao.printSQLException(e);
+    }
+    return logado;
+  }
+
+}
