b0043143ee2b6803e333027e04d3b09e59e2dc91
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-11-21 09:06:55 -0300

    melhorias de lint no codigo

--- src/br/com/trabalhofinal/CadastrarUsuario.java
+++ src/br/com/trabalhofinal/CadastrarUsuario.java
@@ -5,21 +5,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CadastrarUsuario extends Usuario{
+public class CadastrarUsuario extends Usuario {
 
   public CadastrarUsuario(String nome, String email, String senha) {
     super(nome, email, senha);
   }
 
-  public boolean verificaSenha(String confirmaSenha){
-    if(getSenha().equals(confirmaSenha)){
+  public boolean verificaSenha(String confirmaSenha) {
+    if (getSenha().equals(confirmaSenha)) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  public boolean emailEmUso(){
+  public boolean emailEmUso() {
     Connection conn = Conexao.connect();
     boolean emUso = false;
 
@@ -27,19 +27,19 @@ public class CadastrarUsuario extends Usuario{
       PreparedStatement pstmt = conn.prepareStatement("SELECT email FROM usuario WHERE email = ?");
       pstmt.setString(1, getEmail());
       ResultSet rs = pstmt.executeQuery();
-    
+
       if (rs.next()) {
         conn.close();
         emUso = true;
       }
-    
-    } catch (SQLException e) {      
+
+    } catch (SQLException e) {
       Conexao.printSQLException(e);
     }
     return emUso;
   }
-  
-  public void insereUsuario() throws SQLException {    
+
+  public void insereUsuario() throws SQLException {
     Connection conn = Conexao.connect();
     try {
       PreparedStatement pstmt = conn.prepareStatement("INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)");
@@ -47,12 +47,11 @@ public class CadastrarUsuario extends Usuario{
       pstmt.setString(2, getEmail());
       pstmt.setString(3, getSenha());
       pstmt.executeUpdate();
-      
+
     } catch (SQLException e) {
       Conexao.printSQLException(e);
     }
     conn.close();
   }
-
 
 }
