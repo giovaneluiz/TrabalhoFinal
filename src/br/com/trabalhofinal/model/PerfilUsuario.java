8c37350fa1a4906f0566871554f0751b3fbed6a7
Giovane Luiz <giovaneluiz19@yahoo.com.br>
2022-11-30 20:02:08 -0300

    adicionado atributo online na classe PerfilUsuario

--- src/br/com/trabalhofinal/model/PerfilUsuario.java
+++ src/br/com/trabalhofinal/model/PerfilUsuario.java
@@ -1,9 +1,18 @@
 package br.com.trabalhofinal.model;
 
 public class PerfilUsuario extends Usuario {
+  private boolean online;
 
   public PerfilUsuario(int id, String nome, String email) {
     super(id, nome, email);
+  }
+
+  public boolean isOnline() {
+    return online;
+  }
+
+  public void setOnline(boolean online) {
+    this.online = online;
   }
 
 }
