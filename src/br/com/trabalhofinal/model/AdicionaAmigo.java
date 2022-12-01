92a354c47a24e947d2fbbc0eec433ecfc3971c88
giovaneluiz <giovaneluiz19@yahoo.com.br>
2022-12-01 09:13:30 -0300

    correção de bug ao listar usuarios para adicionar como amigo

--- src/br/com/trabalhofinal/model/AdicionaAmigo.java
+++ src/br/com/trabalhofinal/model/AdicionaAmigo.java
@@ -9,9 +9,10 @@ import java.util.ArrayList;
 public class AdicionaAmigo {
   private ArrayList<PerfilUsuario> perfilUsuarios = new ArrayList<PerfilUsuario>();
 
-  private final static String SELECT_PERFIL_QUERY = "SELECT U.id_usuario, TRIM(U.nome) AS nome, U.email FROM usuario U"
-      + " LEFT OUTER JOIN amizade S ON U.id_usuario = S.id_usuario_amigo"
-      + " WHERE S.id_usuario_principal IS null AND U.id_usuario <> ?"
+  private final static String SELECT_PERFIL_QUERY = "SELECT id_usuario, TRIM(nome) AS nome, email FROM usuario"
+      + " WHERE id_usuario NOT IN ("
+      + " SELECT id_usuario_amigo from amizade where id_usuario_principal = ?"
+      + " ) AND id_usuario <> ?"
       + " ORDER BY id_usuario";
 
   private final static String INSERT_AMIZADE_QUERY = "INSERT INTO amizade (id_usuario_principal, id_usuario_amigo) VALUES (?, ?)";
@@ -21,6 +22,7 @@ public class AdicionaAmigo {
       Connection conn = Conexao.connect();
       PreparedStatement pstmt = conn.prepareStatement(SELECT_PERFIL_QUERY);
       pstmt.setInt(1, id_usuario_logado);
+      pstmt.setInt(2, id_usuario_logado);
       ResultSet rs = pstmt.executeQuery();
 
       while (rs.next()) {
